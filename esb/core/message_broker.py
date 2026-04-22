6aff0c9dcd5664106cde9dae9aa9ae2ba186a3d4
NAVANEETH A D <111980664+NAV04@users.noreply.github.com>
2026-04-22 20:25:13 +0530

    Add core module: message_broker.py

---
+++ esb/core/message_broker.py
@@ -0,0 +1,11 @@
+# Message Broker Implementation
+
+class MessageBroker:
+    def __init__(self):
+        pass
+    
+    def publish(self, message):
+        pass
+    
+    def subscribe(self):
+        pass
